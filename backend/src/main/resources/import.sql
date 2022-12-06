ce18169ec40fbe9d196b22265d0f7b16fabe59a2
Fledson Henrique <fledsonhenrique@gmail.com>
2022-12-05 21:40:12 -0300

    :sparkles: feat: created entity course

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2 , 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3 , 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3 , 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3 , 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ASwitch-course-book.svg&psig=AOvVaw3h_-0iMv9y6C3GKcU2T7Wf&ust=1670373206185000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOCPzqDf4_sCFQAAAAAdAAAAABAE', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ASwitch-course-book-grey.svg&psig=AOvVaw3h_-0iMv9y6C3GKcU2T7Wf&ust=1670373206185000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOCPzqDf4_sCFQAAAAAdAAAAABAJ');
